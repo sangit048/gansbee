123bb97cdea08ecbc1a743d0ea99f4b96730f997
sang <sangfs.it@gmail.com>
2026-07-16 20:34:46 +0700

    fix update

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:3001",
       "https://gansbee.onrender.com",
-      "https://gnas-iota.vercel.app",
+      "https://sandclub.vercel.app",
     ],
     credentials: true,
   }),
